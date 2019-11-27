d6fbc245be91aa9335694777d1730258db6fd0b8
ueslei <uesleijf@gmail.com>
2019-11-27 10:28:07 -0300

    - Add Framework Flask and examples

---
+++ Flask/hello4.py
@@ -0,0 +1,18 @@
+from flask import Flask, flash, redirect, render_template, request, session, abort
+ 
+app = Flask(__name__)
+ 
+@app.route("/")
+def index():
+    return "Flask App!"
+
+@app.route("/hello")
+def nome():
+    return "Digite um nome na URL!"
+ 
+@app.route("/hello/<string:name>/")
+def hello(name):
+    return render_template('template2.html',name=name)
+ 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=90)
